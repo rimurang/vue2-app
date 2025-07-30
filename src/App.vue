2e042854779b16d0cd608e86030b20025e1717eb
rimurang <dkffkqb000@gmail.com>
2025-07-30 15:41:52 +0900

    2차, aiox 활용성

--- src/App.vue
+++ src/App.vue
@@ -21,20 +21,38 @@
         />
       </ul>
     </div>
+
+    <div class="todo-container">
+      <h2>외부에서 불러온 할 일 목록</h2>
+      <button @click="fetchTodos">불러오기</button>
+      <ul class="todo-list apiTodo">
+        <ApiTodoItem
+          v-for="todo in apiTodos"
+          :key="todo.id"
+          :content="todo.title"
+          @delete="removeApiTodo(todo.id)"
+        />
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import TodoItem from "./components/TodoItem.vue";
 
+import axios from "axios";
+import ApiTodoItem from "./components/ApiTodoItem.vue";
+
 export default {
   components: {
     TodoItem,
+    ApiTodoItem,
   },
   data() {
     return {
       newTodo: "",
       todos: [],
+      apiTodos: [],
     };
   },
   methods: {
@@ -46,7 +64,24 @@ export default {
       }
     },
     removeTodo(index) {
-      this.todos.splice(index, 1);
+      this.todos.splice(index, 1); //splice는 원본 배열 훼손 수정
+    },
+
+    async fetchTodos() {
+      try {
+        const res = await axios.get(
+          "https://jsonplaceholder.typicode.com/todos?_limit=5"
+        );
+        this.apiTodos = res.data; // res 의 데이터 객체or배열 가져와서 this.apiTodos 에 담기
+        console.log(this.apiTodos);
+      } catch (err) {
+        alert("데이터를 불러오는 데 실패했습니다.");
+      }
+    },
+    removeApiTodo(id) {
+      //this.apiTodos.splice(id, 1); // < splice는 원본 훼손
+      this.apiTodos = this.apiTodos.filter((todo) => todo.id != id); // filter는 원본 훼손 없이 새로운 배열or객체 반환 (콜백함수)
+      // id 가 같은 애는 제외하고 아닌애들만 모아서 새로운 배열 반환
     },
   },
 };
@@ -85,12 +120,13 @@ export default {
 }
 
 .form button {
-  padding: 8px 16px;
+  padding: 8px 0;
   background-color: #42b983;
   color: white;
   border: none;
   border-radius: 6px;
   cursor: pointer;
+  width: 80px;
 }
 
 .form button:hover {
---
+++ src/components/ApiTodoItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <li class="todo-item">
+    {{ content }}
+    <button @click="$emit('delete')">삭제</button>
+  </li>
+</template>
+
+<script>
+export default {
+  // 부모한테서 받 아옴
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.todo-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 6px 0;
+  border-bottom: 1px dashed #ddd;
+}
+.todo-item button {
+  background: #ff5c5c;
+  color: white;
+  border: none;
+  padding: 4px 10px;
+  border-radius: 6px;
+  cursor: pointer;
+}
+.todo-item button:hover {
+  background: #e14e4e;
+}
+</style>
